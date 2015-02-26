b040cebf2561d0d973d9e5ed58d95998927dbc1c
nitrotron <toddmetcalf@gmail.com>
2015-02-26 07:41:22 -0500

    Added command object

--- ngBrewduino/App/brewduino/brewduinoCmdVal.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="../../3rdParty/script/angular.intellisense.js" />
-/// <reference path="../../3rdParty/script/angular.js" />
-// This is a value service for defining the commands to be sent to the arduino
-(function () {
-
-})();
