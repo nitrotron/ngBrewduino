b217ae5acdbe930eacbd598b73bca38ecc564dd5
nitrotron <toddmetcalf@gmail.com>
2015-03-13 15:57:45 -0400

    Added more data, and data is more brew specific

--- ngBrewduino/App/brewduinoStubData.js
+++ ngBrewduino/App/brewduinoStubData.js
@@ -8,32 +8,32 @@
     chartData.data = {
         'cols': [
           {
-              'id': 'month',
-              'label': 'Month',
+              'id': 'Time',
+              'label': 'Time',
               'type': 'string',
               'p': {}
           },
           {
-              'id': 'laptop-id',
-              'label': 'Laptop',
+              'id': 't0',
+              'label': 'RIMS',
               'type': 'number',
               'p': {}
           },
           {
-              'id': 'desktop-id',
-              'label': 'Desktop',
+              'id': 't1',
+              'label': 'Kettle',
               'type': 'number',
               'p': {}
           },
           {
-              'id': 'server-id',
-              'label': 'Server',
+              'id': 't2',
+              'label': 'Mash',
               'type': 'number',
               'p': {}
           },
           {
-              'id': 'cost-id',
-              'label': 'Shipping',
+              'id': 't3',
+              'label': 'HLT/Kettle2',
               'type': 'number'
           }
         ],
@@ -41,113 +41,172 @@
           {
               'c': [
                 {
-                    'v': 'January'
+                    'v': '10:30'
                 },
                 {
-                    'v': 19,
-                    'f': '42 items'
+                    'v': 115
                 },
                 {
-                    'v': 12,
-                    'f': 'Ony 12 items'
+                    'v': 86
+
                 },
                 {
-                    'v': 7,
-                    'f': '7 servers'
+                    'v': 100
                 },
                 {
-                    'v': 4
+                    'v': 200
                 }
               ]
           },
           {
               'c': [
                 {
-                    'v': 'February'
+                    'v': '10:30'
                 },
                 {
-                    'v': 13
+                    'v': 120
                 },
                 {
-                    'v': 1,
-                    'f': '1 unit (Out of stock this month)'
+                    'v': 78
                 },
                 {
-                    'v': 12
+                    'v': 100
                 },
                 {
-                    'v': 2
+                    'v': 198
                 }
               ]
           },
           {
               'c': [
                 {
-                    'v': 'March'
+                    'v': '10:32'
                 },
                 {
-                    'v': 24
+                    'v': 125
                 },
                 {
-                    'v': 5
+                    'v': 99
                 },
                 {
-                    'v': 11
+                    'v': 103
                 },
                 {
-                    'v': 6
+                    'v': 195
+                }
+              ]
+          },
+
+          {
+              'c': [
+                {
+                    'v': '10:33'
+                },
+                {
+                    'v': 125
+                },
+                {
+                    'v': 70
+                },
+                {
+                    'v': 103
+                },
+                {
+                    'v': 197
+                }
+              ]
+          },
+          {
+              'c': [
+                {
+                    'v': '10:34'
+                },
+                {
+                    'v': 125
+                },
+                {
+                    'v': 70
+                },
+                {
+                    'v': 103
+                },
+                {
+                    'v': 197
+                }
+              ]
+          },
+          {
+              'c': [
+                {
+                    'v': '10:35'
+                },
+                {
+                    'v': 125
+                },
+                {
+                    'v': 70
+                },
+                {
+                    'v': 103
+                },
+                {
+                    'v': 197
                 }
               ]
           }
         ]
     };
 
-    
+
 
     chartData.options = {
-        'title': 'Sales per month',
+        'title': 'Temperatures',
         'isStacked': 'true',
         'fill': 20,
+        'curveType': 'function',
+        'trendlines' : {
+            1: {}
+        },
         'displayExactValues': true,
         'vAxis': {
-            'title': 'Sales unit', 'gridlines': { 'count': 6 }
+            'title': 'Temperature unit', 'gridlines': { 'count': 6 }
         },
         'hAxis': {
             'title': 'Date'
         }
-    };
+};
 
-    chartData.formatters = {};
+chartData.formatters = {};
 
 
-    var stubData = {
-        thermometers: [
-            { id: 0, temp: 100, highAlarm: 200, lowAlarm: 15, name: 'RIMs', isRIMS: true },
-            { id: 1, temp: 110, highAlarm: 210, lowAlarm: 25, name: 'Mash' },
-            { id: 2, temp: 120, highAlarm: 220, lowAlarm: 35, name: 'HLT' },
-            { id: 3, temp: 130, highAlarm: 230, lowAlarm: 45, name: 'Kettle' }
-        ],
+var stubData = {
+    thermometers: [
+        { id: 0, temp: 100, highAlarm: 200, lowAlarm: 15, name: 'RIMs', isRIMS: true },
+        { id: 1, temp: 110, highAlarm: 210, lowAlarm: 25, name: 'Mash' },
+        { id: 2, temp: 120, highAlarm: 220, lowAlarm: 35, name: 'HLT' },
+        { id: 3, temp: 130, highAlarm: 230, lowAlarm: 45, name: 'Kettle' }
+    ],
 
-        //Thermometer0: 100,
-        //ThermometerHighAlarm0: 200,
-        //ThermometerLowAlarm0: 40,
-        TotalTimers: 12,
-        RimsEnable: false,
-        AuxOn: false,
-        PumpOn: false,
-        WhichThermoAlarm: 'kettle',
-        TimersNotAllocated: 3,
-        Kp: 200,
-        Ki: 1,
-        kd: 1,
-        SetPoint: 100,
-        WindowSize: 5000,
-        TempAlarmActive: false,
-        TimerAlarmActive: false
+    //Thermometer0: 100,
+    //ThermometerHighAlarm0: 200,
+    //ThermometerLowAlarm0: 40,
+    TotalTimers: 12,
+    RimsEnable: false,
+    AuxOn: false,
+    PumpOn: false,
+    WhichThermoAlarm: 'kettle',
+    TimersNotAllocated: 3,
+    Kp: 200,
+    Ki: 1,
+    kd: 1,
+    SetPoint: 100,
+    WindowSize: 5000,
+    TempAlarmActive: false,
+    TimerAlarmActive: false
 
-    };
+};
 
-    angular.module('app')
-        .value('stubData', stubData)
-        .value('chartData', chartData);
+angular.module('app')
+    .value('stubData', stubData)
+    .value('chartData', chartData);
 })();
