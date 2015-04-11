99c9bedde477f792deace642fe3117086d5d7a3d
nitrotron <toddmetcalf@gmail.com>
2015-04-10 22:27:11 -0400

    initialdrop

--- src/server/serialTest.js
+++ src/server/serialTest.js
@@ -8,3 +8,23 @@ var myPort = new SerialPort(portName, {
    // look for return and newline at the end of each data packet:
    parser: serialport.parsers.readline("\r\n")
  });
+
+myPort.on('open', showPortOpen);
+    myPort.on('data', saveLatestData);
+    myPort.on('error', showError);
+
+function showPortOpen() {
+        console.log('port open. Data rate: ' + myPort.options.baudRate);
+    }
+
+ // this is called when the serial port has an error:
+    function showError(error) {
+        console.log('Serial port error: ' + error);
+    }
+
+// this is called when new data comes into the serial port:
+    function saveLatestData(data) {
+        // save the incoming serial data in serialData variable
+        console.log('just recieved: ' + data);
+        
+    }
