c09f1560832e7e99c30df62834950593239d322c
nitrotron <toddmetcalf@gmail.com>
2015-04-19 07:44:57 -0400

    made font larger

--- src/client/App/settings/settingsSrv.js
+++ src/client/App/settings/settingsSrv.js
@@ -15,12 +15,12 @@
 
     };
     settingsSrv.thermos = [
-        { name: 'HLT / Kettle2', order: 0, chartEnabled: false },
-        { name: 'Mash', order: 1, chartEnabled: false },
-        { name: 'Kettle', order: 2, chartEnabled: false },
+        { name: 'Mash', order: 0, chartEnabled: false },
+        { name: 'Kettle', order: 1, chartEnabled: false },
+        { name: 'HLT / Kettle2', order: 2, chartEnabled: false },
         { name: 'RIMS', order: 3, chartEnabled: false },
 
     ];
-    settingsSrv.thermoNames = ['HLT / Kettle2', 'Mash', 'Kettle', 'RIMS'];
+    settingsSrv.thermoNames = ['Mash', 'Kettle','HLT / Kettle2', 'RIMS'];
 
 })();
