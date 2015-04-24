3aee4522c0e1e0b152f4e5ae98147dfab1a08400
nitrotron <toddmetcalf@gmail.com>
2015-04-23 21:02:39 -0400

    put the chart update back to 60 sec

--- src/client/App/chart/chartSrv.js
+++ src/client/App/chart/chartSrv.js
@@ -24,7 +24,7 @@
                 if (autoUpdatesEnabled === true) {
                     getChartData();
                 }
-            }, 3000);
+            }, 60000);
         }
 
         function getChartConfig() {
