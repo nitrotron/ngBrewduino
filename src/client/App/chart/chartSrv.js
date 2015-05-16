5d89806d5fe16686b8c1730596c4fdaad00646d1
nitrotron <toddmetcalf@gmail.com>
2015-05-16 09:05:51 -0400

    b

--- src/client/App/chart/chartSrv.js
+++ src/client/App/chart/chartSrv.js
@@ -105,7 +105,7 @@
                 'yAxis': [{
                     title: {
                         text: 'Temperatures'
-                    },
+                    }
 
                 },
                 {
