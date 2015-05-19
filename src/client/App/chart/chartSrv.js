e3f6edec1127b9e1194c50ec65277fcda34ce342
nitrotron <toddmetcalf@gmail.com>
2015-05-19 17:18:24 -0400

    changes to chart theme

--- src/client/App/chart/chartSrv.js
+++ src/client/App/chart/chartSrv.js
@@ -33,6 +33,7 @@
             //        getChartData();
             //    }
             //}, 60000);
+
         }
 
         function activate() {
@@ -80,218 +81,33 @@
         }
 
         function getChartConfig() {
+            // var colors = ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+            //"#55BF3B", "#DF5353", "#7798BF", "#aaeeee"];
             chartConfig = {
-                'theme': {
-                    colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
-                       "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
-                    chart: {
-                        backgroundColor: {
-                            linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-                            stops: [
+                'options': {
+                    'chart': {
+                        'type': 'spline',
+                        'zoomType': 'x',
+                        'backgroundColor': {
+                            'linearGradient': { x1: 0, y1: 0, x2: 1, y2: 1 },
+                            'stops': [
                                [0, '#2a2a2b'],
                                [1, '#3e3e40']
                             ]
                         },
-                        style: {
-                            fontFamily: "'Unica One', sans-serif"
+                        'plotOptions': {
+                            'series': {
+                                'stacking': '',
+                                'marker': {
+                                    'radius': 1
+                                }
+                            }
+
                         },
                         plotBorderColor: '#606063'
                     },
-                    title: {
-                        style: {
-                            color: '#E0E0E3',
-                            textTransform: 'uppercase',
-                            fontSize: '20px'
-                        }
-                    },
-                    subtitle: {
-                        style: {
-                            color: '#E0E0E3',
-                            textTransform: 'uppercase'
-                        }
-                    },
-                    xAxis: {
-                        gridLineColor: '#707073',
-                        labels: {
-                            style: {
-                                color: '#E0E0E3'
-                            }
-                        },
-                        lineColor: '#707073',
-                        minorGridLineColor: '#505053',
-                        tickColor: '#707073',
-                        title: {
-                            style: {
-                                color: '#A0A0A3'
-
-                            }
-                        }
-                    },
-                    yAxis: {
-                        gridLineColor: '#707073',
-                        labels: {
-                            style: {
-                                color: '#E0E0E3'
-                            }
-                        },
-                        lineColor: '#707073',
-                        minorGridLineColor: '#505053',
-                        tickColor: '#707073',
-                        tickWidth: 1,
-                        title: {
-                            style: {
-                                color: '#A0A0A3'
-                            }
-                        }
-                    },
-                    tooltip: {
-                        backgroundColor: 'rgba(0, 0, 0, 0.85)',
-                        style: {
-                            color: '#F0F0F0'
-                        }
-                    },
-                    plotOptions: {
-                        series: {
-                            dataLabels: {
-                                color: '#B0B0B3'
-                            },
-                            marker: {
-                                lineColor: '#333'
-                            }
-                        },
-                        boxplot: {
-                            fillColor: '#505053'
-                        },
-                        candlestick: {
-                            lineColor: 'white'
-                        },
-                        errorbar: {
-                            color: 'white'
-                        }
-                    },
-                    legend: {
-                        itemStyle: {
-                            color: '#E0E0E3'
-                        },
-                        itemHoverStyle: {
-                            color: '#FFF'
-                        },
-                        itemHiddenStyle: {
-                            color: '#606063'
-                        }
-                    },
-                    credits: {
-                        style: {
-                            color: '#666'
-                        }
-                    },
-                    labels: {
-                        style: {
-                            color: '#707073'
-                        }
-                    },
-
-                    drilldown: {
-                        activeAxisLabelStyle: {
-                            color: '#F0F0F3'
-                        },
-                        activeDataLabelStyle: {
-                            color: '#F0F0F3'
-                        }
-                    },
-
-                    navigation: {
-                        buttonOptions: {
-                            symbolStroke: '#DDDDDD',
-                            theme: {
-                                fill: '#505053'
-                            }
-                        }
-                    },
-
-                    // scroll charts
-                    rangeSelector: {
-                        buttonTheme: {
-                            fill: '#505053',
-                            stroke: '#000000',
-                            style: {
-                                color: '#CCC'
-                            },
-                            states: {
-                                hover: {
-                                    fill: '#707073',
-                                    stroke: '#000000',
-                                    style: {
-                                        color: 'white'
-                                    }
-                                },
-                                select: {
-                                    fill: '#000003',
-                                    stroke: '#000000',
-                                    style: {
-                                        color: 'white'
-                                    }
-                                }
-                            }
-                        },
-                        inputBoxBorderColor: '#505053',
-                        inputStyle: {
-                            backgroundColor: '#333',
-                            color: 'silver'
-                        },
-                        labelStyle: {
-                            color: 'silver'
-                        }
-                    },
-
-                    navigator: {
-                        handles: {
-                            backgroundColor: '#666',
-                            borderColor: '#AAA'
-                        },
-                        outlineColor: '#CCC',
-                        maskFill: 'rgba(255,255,255,0.1)',
-                        series: {
-                            color: '#7798BF',
-                            lineColor: '#A6C7ED'
-                        },
-                        xAxis: {
-                            gridLineColor: '#505053'
-                        }
-                    },
-
-                    scrollbar: {
-                        barBackgroundColor: '#808083',
-                        barBorderColor: '#808083',
-                        buttonArrowColor: '#CCC',
-                        buttonBackgroundColor: '#606063',
-                        buttonBorderColor: '#606063',
-                        rifleColor: '#FFF',
-                        trackBackgroundColor: '#404043',
-                        trackBorderColor: '#404043'
-                    },
-
-                    // special colors for some of the
-                    legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    background2: '#505053',
-                    dataLabelsColor: '#B0B0B3',
-                    textColor: '#C0C0C0',
-                    contrastTextColor: '#F0F0F3',
-                    maskColor: 'rgba(255,255,255,0.3)'
-                }, //end of theme
-                'options': {
-                    'chart': {
-                        'type': 'spline',
-                        'zoomType': 'x'
-                    },
-                    'plotOptions': {
-                        'series': {
-                            'stacking': '',
-                            'marker': {
-                                'radius': 2
-                            }
-                        }
-                    }
+                    colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+                                "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"]
                 },
                 'xAxis': {
                     type: 'datetime',
@@ -301,72 +117,110 @@
                         hour: '%H:%M',
                     },
                     title: {
-                        text: 'Time'
+                        text: 'Time',
+                        style: {
+                            color: '#A0A0A3'
+
+                        }
                     },
-                    minRange: 60000 * 3 // 3 minutes
+                    minRange: 60000 * 3, // 3 minutes
+                    gridLineColor: '#707073',
+                    labels: {
+                        style: {
+                            color: '#E0E0E3'
+                        }
+                    },
+                    lineColor: '#707073',
+                    minorGridLineColor: '#505053',
+                    tickColor: '#707073'
 
                 },
                 'yAxis': [{
+                    gridLineColor: '#707073',
+                    labels: {
+                        style: {
+                            color: '#E0E0E3'
+                        }
+                    },
+                    lineColor: '#707073',
+                    minorGridLineColor: '#505053',
+                    tickColor: '#707073',
+                    tickWidth: 1,
                     title: {
-                        text: 'Temperatures'
+                        text: 'Temperatures',
+                        style: {
+                            color: '#A0A0A3'
+                        }
                     },
                     min: 64
 
                 },
                 {
+                    gridLineColor: '#707073',
+                    labels: {
+                        style: {
+                            color: '#E0E0E3'
+                        }
+                    },
+                    lineColor: '#707073',
+                    minorGridLineColor: '#505053',
+                    tickColor: '#707073',
+                    tickWidth: 1,
                     title: {
-                        text: 'RIMs Window'
+                        text: 'RIMs Window',
+                        style: {
+                            color: '#A0A0A3'
+                        }
                     },
                     opposite: true
                 }],
                 'series': [
-    {
-        'name': settingsSrv.thermos[0].name,
-        'data': [
-        ],
-        'id': 0
-    },
-    {
-        'name': settingsSrv.thermos[1].name,
-        'data': [
-        ],
-        'id': 1
-    },
-    {
-        'name': settingsSrv.thermos[2].name,
-        'data': [
-        ],
-        'id': 2
-    },
-    {
-        'name': settingsSrv.thermos[3].name,
-        'data': [
-        ],
-        'id': 3
-    },
-    {
-        'name': 'RIMs Setpoint',
-        'data': [
-        ],
-        'id': 4,
-        'visible': false
-    },
-    {
-        'name': 'RIMS Window',
-        'data': [
-        ],
-        'id': 5,
-        'type': 'column',
-        'yAxis': 1,
-        'visible': false
-    }
+                            {
+                                'name': settingsSrv.thermos[0].name,
+                                'data': [],
+                                'id': 0
+                            },
+                            {
+                                'name': settingsSrv.thermos[1].name,
+                                'data': [],
+                                'id': 1
+                            },
+                            {
+                                'name': settingsSrv.thermos[2].name,
+                                'data': [],
+                                'id': 2
+                            },
+                            {
+                                'name': settingsSrv.thermos[3].name,
+                                'data': [],
+                                'id': 3
+                            },
+                            {
+                                'name': 'RIMs Setpoint',
+                                'data': [],
+                                'id': 4,
+                                'visible': false
+                            },
+                            {
+                                'name': 'RIMS Window',
+                                'data': [],
+                                'id': 5,
+                                'type': 'column',
+                                'yAxis': 1,
+                                'visible': false
+                            }
                 ],
 
                 'title': {
-                    'text': 'Hello'
+                    'text': 'Hello',
+                    'style': {
+                        color: '#E0E0E3',
+                        textTransform: 'uppercase',
+                        fontSize: '20px'
+                    }
                 },
                 'credits': {
-                    'enabled': false
+                    'enabled': true
                 },
                 'loading': false,
                 'size': {
@@ -374,7 +228,11 @@
                     'height': '400'
                 },
                 'subtitle': {
-                    'text': 'this is a subtitle'
+                    'text': 'this is a subtitle',
+                    'style': {
+                        color: '#E0E0E3',
+                        textTransform: 'uppercase'
+                    }
                 }
             };
 
