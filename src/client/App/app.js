79ce06e2a5e783c1f1827e705760eba1a4006a2b
nitrotron <toddmetcalf@gmail.com>
2015-10-22 11:59:00 -0400

    added a new workaround for chrome on IOS9.

--- src/client/App/app.js
+++ src/client/App/app.js
@@ -2,5 +2,5 @@
 /// <reference path='../3rdParty/script/angular-ui-router.min.js' />
 (function () {
     angular.module('app', ['timer', 'ui.router', 'googlechart', 'ngAnimate',
-        'toaster', 'app.logger', 'ngAnimate', 'highcharts-ng']);
+        'toaster', 'app.logger', 'ngAnimate', 'highcharts-ng', 'ngIOS9UIWebViewPatch']);
 }());
